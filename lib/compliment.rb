4fc48da32d4975cdd7c82b3162673cec2ff9c618
Jonathan Brown <jonathanbrown.a@gmail.com>
2013-12-09 16:28:15 -0500

    add compliments.txt and compliment generator

--- lib/compliment.rb
+++ lib/compliment.rb
@@ -1,11 +1,22 @@
 class Compliment
+  attr_reader :message, :image
 	def initialize
-		@message
+		@message = compliment_generator
 		@image = grab_random_image
 	end
 
 	def grab_random_image
         images = Dir.entries("./public/images").select {|f| !File.directory? f}
-        images[rand(images.length)]
 	end
+
+  def compliment_generator
+    compliments = Array.new
+      File.open("./compliments.txt") do |file|
+          file.each_line do |line|
+            matches = line.match(/(\d*.\s)(.*)/)
+            compliments << matches[2]
+          end
+      end
+    compliments
+  end
 end
