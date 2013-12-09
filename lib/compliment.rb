1de67e4f42ad846a105e8b78325ef12f58c58046
Kphillycat <keithwilliamsjr@gmail.com>
2013-12-09 16:04:43 -0500

    Add random image method to compliment.rb

--- lib/compliment.rb
+++ lib/compliment.rb
@@ -1,6 +1,11 @@
 class Compliment
 	def initialize
 		@message
-		@image
+		@image = grab_random_image
+	end
+
+	def grab_random_image
+        images = Dir.entries("./public/images").select {|f| !File.directory? f}
+        images[rand(images.length)]
 	end
 end
